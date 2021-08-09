bfff3bc72937020d1e18318cec3356235717a593
Konvoir <baleskoff@gmail.com>
2021-08-09 18:45:01 +0300

    layout the home page container for correct positioning of cards

---
+++ src/js/fetchapi.js
@@ -0,0 +1,3 @@
+const API_KEY = '93ab5af0b6f3bcde1224ca161062db06';
+const BASE_URL = `https://www.api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
+const ALT_URL = `https://www.api.themoviedb.org/t/p/w500?api_key=${API_KEY}`;
